3b43b5f8333df17508c161ae7dc432ee55b1dde9
Vasily Palkov <vasilypalkov@gmail.com>
2023-01-10 22:01:36 +0400

    Add project

---
+++ src/main/java/com/arguvos/yourtopwords/YourTopWordsApplication.java
@@ -0,0 +1,13 @@
+package com.arguvos.yourtopwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourTopWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourTopWordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arguvos/yourtopwords/YourTopWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arguvos.yourtopwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourTopWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
